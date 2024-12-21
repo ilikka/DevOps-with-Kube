adb8db431c0fcbfacea8f09018019d8b610942b7
ilikka <47757386+ilikka@users.noreply.github.com>
2024-12-21 18:14:12 +0200

    logging info

--- chapter_01/Exercise_1.01/hash.py
+++ chapter_01/Exercise_1.01/hash.py
@@ -1,15 +1,19 @@
 import hashlib
 import time 
+import logging
 from datetime import datetime
 
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+
+pseudo_random = datetime.now().strftime('%Y-%m-%d-%s-%H:%M')
+hash_object = hashlib.sha256(pseudo_random.encode())
+hash_hex = hash_object.hexdigest()
 
 while True: 
   current_datetime = datetime.now()
 
-  datetime_str = str(current_datetime.strftime('%Y-%m-%d %H:%M') )
+  datetime_str = str(current_datetime.strftime('%Y-%m-%d %H:%M.%S') )
 
-  hash_object = hashlib.sha256(datetime_str.encode())
-  hash_hex = hash_object.hexdigest()
-
-  print(datetime_str, ":", hash_hex)
+  logging.info(f"{datetime_str} : {hash_hex}" )
   time.sleep(5)
