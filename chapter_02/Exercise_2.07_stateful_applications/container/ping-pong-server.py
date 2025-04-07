f4e225d06c033039ff61655361af151f10d0d608
ilikka <47757386+ilikka@users.noreply.github.com>
2025-04-07 19:07:34 +0300

    chapter_02  ex 2.07

--- chapter_02/Exercise_2.07_stateful_applications/container/ping-pong-server.py
+++ chapter_02/Exercise_2.07_stateful_applications/container/ping-pong-server.py
@@ -1,10 +1,18 @@
 import os
 import logging
 from flask import Flask, jsonify
+import psycopg2
 
 
 # Create a Flask application
 app = Flask(__name__)
+
+# Database connection parameters
+DB_HOST = os.getenv('DB_HOST','postgres-service.devops-ns.svc.cluster.local')  # Change if needed
+DB_PORT = "5432"
+DB_NAME = "devopsdb"
+DB_USER = "admin"
+DB_PASSWORD = "password"
 
 # Read environment variables with defaults
 ping_path = os.getenv('PING_PATH', '/ping')
@@ -13,18 +21,34 @@ port = int(os.getenv('FLASK_PORT', 5000))
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-request_counter = 0 
+def get_nextval():
+    """Fetch the next value of the sequence from PostgreSQL."""
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT nextval('select_counter_seq');")
+        result = cur.fetchone()[0]  # Get the first column of the first row
+        cur.close()
+        conn.close()
+        return result
+    except Exception as e:
+        return str(e)
 
 
 # Define a route that listens to GET requests at the URL specified by the environment variable
 @app.route(ping_path, methods=['GET'])
 def pong():
-    
-    global request_counter
-    request_counter  += 1 
+    """Handle GET requests and return the next sequence value."""
+     
+    request_counter  = get_nextval()
     logging.info(f"request counter: '{request_counter}' ")
     return f"{request_counter}", 200
-
 
 # Default route to catch all undefined URLs and return a custom "Not Found" message
 @app.route('/<path:path>', methods=['GET'])
